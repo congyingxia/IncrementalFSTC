c17f10dcb5296eee452f777869122c05f71d8553
Congying <cxia8@uic.edu>
2021-04-27 17:58:19 -0500

    add code

--- code/With_base_Intent/train.dynamicFewShot.commands.sh
+++ code/With_base_Intent/train.dynamicFewShot.commands.sh
@@ -15,65 +15,65 @@ CUDA_VISIBLE_DEVICES=0 python -u train.dynamicFewShot.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 64 \
     --seed $SEED \
-    --round_name base > log.dynamicFewShot.base.seed.$SEED.debug.txt 2>&1 &
+    --round_name base > log.dynamicFewShot.base.seed.$SEED.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u train.dynamicFewShot.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
-    --seed $SEED \
-    --round_name r1 > log.dynamicFewShot.r1.seed.$SEED.debug.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=2 python -u train.dynamicFewShot.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
-    --seed $SEED \
-    --round_name r2 > log.dynamicFewShot.r2.seed.$SEED.debug.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=3 python -u train.dynamicFewShot.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
-    --seed $SEED \
-    --round_name r3 > log.dynamicFewShot.r3.seed.$SEED.debug.txt 2>&1 &
-
-
-CUDA_VISIBLE_DEVICES=4 python -u train.dynamicFewShot.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
-    --seed $SEED \
-    --round_name r4 > log.dynamicFewShot.r4.seed.$SEED.debug.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=5 python -u train.dynamicFewShot.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 64 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 64 \
-    --seed $SEED \
-    --round_name r5 > log.dynamicFewShot.r5.seed.$SEED.debug.txt 2>&1 &
+#CUDA_VISIBLE_DEVICES=1 python -u train.dynamicFewShot.py \
+#    --task_name rte \
+#    --do_train \
+#    --do_lower_case \
+#    --num_train_epochs $EPOCHSIZE \
+#    --train_batch_size $BATCHSIZE \
+#    --eval_batch_size 64 \
+#    --learning_rate $LEARNINGRATE \
+#    --max_seq_length 64 \
+#    --seed $SEED \
+#    --round_name r1 > log.dynamicFewShot.r1.seed.$SEED.txt 2>&1 &
+#
+#CUDA_VISIBLE_DEVICES=2 python -u train.dynamicFewShot.py \
+#    --task_name rte \
+#    --do_train \
+#    --do_lower_case \
+#    --num_train_epochs $EPOCHSIZE \
+#    --train_batch_size $BATCHSIZE \
+#    --eval_batch_size 64 \
+#    --learning_rate $LEARNINGRATE \
+#    --max_seq_length 64 \
+#    --seed $SEED \
+#    --round_name r2 > log.dynamicFewShot.r2.seed.$SEED.txt 2>&1 &
+#
+#CUDA_VISIBLE_DEVICES=3 python -u train.dynamicFewShot.py \
+#    --task_name rte \
+#    --do_train \
+#    --do_lower_case \
+#    --num_train_epochs $EPOCHSIZE \
+#    --train_batch_size $BATCHSIZE \
+#    --eval_batch_size 64 \
+#    --learning_rate $LEARNINGRATE \
+#    --max_seq_length 64 \
+#    --seed $SEED \
+#    --round_name r3 > log.dynamicFewShot.r3.seed.$SEED.txt 2>&1 &
+#
+#
+#CUDA_VISIBLE_DEVICES=4 python -u train.dynamicFewShot.py \
+#    --task_name rte \
+#    --do_train \
+#    --do_lower_case \
+#    --num_train_epochs $EPOCHSIZE \
+#    --train_batch_size $BATCHSIZE \
+#    --eval_batch_size 64 \
+#    --learning_rate $LEARNINGRATE \
+#    --max_seq_length 64 \
+#    --seed $SEED \
+#    --round_name r4 > log.dynamicFewShot.r4.seed.$SEED.txt 2>&1 &
+#
+#CUDA_VISIBLE_DEVICES=5 python -u train.dynamicFewShot.py \
+#    --task_name rte \
+#    --do_train \
+#    --do_lower_case \
+#    --num_train_epochs $EPOCHSIZE \
+#    --train_batch_size $BATCHSIZE \
+#    --eval_batch_size 64 \
+#    --learning_rate $LEARNINGRATE \
+#    --max_seq_length 64 \
+#    --seed $SEED \
+#    --round_name r5 > log.dynamicFewShot.r5.seed.$SEED.txt 2>&1 &
